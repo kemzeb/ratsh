413b0717ab6a05d57edf7cb84f746d12dabba81d
Kemal Zebari <kemalzebra@gmail.com>
2023-08-22 09:45:48 -0700

    feat: Support `cd -`

--- Builtins.cpp
+++ Builtins.cpp
@@ -37,7 +37,23 @@ int builtin_cd(std::vector<std::string> const& argv)
 
     /// FIXME: Implement step 5 (utilizing CDPATH env var).
     /// FIXME: Implement step 7 once an argument parser has been implemented.
-    /// FIXME: Support the '-' operand.
+
+    auto const* pwd = std::getenv("PWD");
+    if (pwd == nullptr) {
+        std::cerr << "failed to get $PWD: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    auto using_old_pwd = false;
+    if (path == "-") {
+        auto const* old_pwd = std::getenv("OLDPWD");
+        if (old_pwd == nullptr) {
+            std::cerr << "$OLDPWD is not set\n";
+            return 1;
+        }
+        path = old_pwd;
+        using_old_pwd = true;
+    }
 
     std::error_code ec;
 
@@ -47,26 +63,22 @@ int builtin_cd(std::vector<std::string> const& argv)
         return 1;
     }
 
-    auto* cwd = std::getenv("PWD");
-    if (cwd == nullptr) {
-        std::cerr << "failed to get PWD: " << strerror(errno) << "\n";
-        return 1;
-    }
-
     auto new_pwd = path;
-    path = std::filesystem::relative(path, cwd, ec);
+
+    path = std::filesystem::relative(path, pwd, ec);
     if (ec) {
         std::cerr << "failed to create relative path: " << ec.message() << "\n";
         return 1;
     }
-
     if (chdir(path.c_str()) == -1) {
-        std::cerr << "chdir() failed" << strerror(errno) << "\n";
+        std::cerr << path << ": " << strerror(errno) << "\n";
         return 1;
     }
+    if (using_old_pwd)
+        std::cout << new_pwd << "\n";
 
     setenv("PWD", new_pwd.c_str(), 1);
-    setenv("OLDPWD", cwd, 1);
+    setenv("OLDPWD", pwd, 1);
 
     return 0;
 }
